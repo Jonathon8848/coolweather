011938385c2773ba1a59b548ac61a8d58d7fdebf
Jonathon <Jonathon@gmail.com>
2019-12-02 21:48:49 +0800

    修改程序图标和名称。

--- app/src/main/java/com/example/lab509/coolweather/WeatherActivity.java
+++ app/src/main/java/com/example/lab509/coolweather/WeatherActivity.java
@@ -1,5 +1,6 @@
 package com.example.lab509.coolweather;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 import android.support.v4.view.GravityCompat;
@@ -20,6 +21,7 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.example.lab509.coolweather.gson.Forecast;
 import com.example.lab509.coolweather.gson.Weather;
+import com.example.lab509.coolweather.service.AutoUpdateService;
 import com.example.lab509.coolweather.util.HttpUtil;
 import com.example.lab509.coolweather.util.Utility;
 
@@ -236,5 +238,7 @@ public class WeatherActivity extends AppCompatActivity {
         carWashText.setText(carWash);
         sportText.setText(sport);
         weatherLayout.setVisibility(View.VISIBLE);
+        Intent intent = new Intent(this, AutoUpdateService.class);
+        startService(intent);
     }
 }
